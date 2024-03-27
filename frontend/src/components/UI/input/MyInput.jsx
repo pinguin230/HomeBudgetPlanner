95ed0bc912b5e052b328844d9be37c2d9eccf522
pinguin230 <tesliamain@gmail.com>
2024-03-27 23:15:16 +0200

    Adding custom component for input

---
+++ frontend/src/components/UI/input/MyInput.jsx
@@ -0,0 +1,9 @@
+// MyInput компонента
+import React from "react";
+import classes from "./MyInput.module.css";
+
+const MyInput = (props) => {
+  return <input {...props} />;
+};
+
+export default MyInput;
